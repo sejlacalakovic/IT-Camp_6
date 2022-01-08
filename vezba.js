af4bcb15b4d4aae26e98dff0b0db5e0f48b87c9b
Sejla <calakovicsejla6@gmail.com>
2022-01-08 18:56:54 +0100

    novo

---
+++ vezba.js
@@ -0,0 +1,326 @@
+// a = window.prompt("Unesite broj:");
+// if (a % 2 === 0) {
+//     console.log("Broj je paran");
+// } else {
+//     console.log("Broj je neparan")
+// }
+
+
+// a = window.prompt("unesite godinu rodjenja");
+// b = window.prompt("unesite mesec rodjenja");
+// c = window.prompt("unesite dan rodjenja");
+// a = 2001
+// b = 2
+// c = 5
+//  console.log(2021-a);
+// console.log(12-b);
+// console.log(28-c);
+
+//  a = window.prompt("Unesite broj");
+//  if (a == 0) {
+//      console.log("Unesite broj je jednak nuli");
+//  }
+//   if (a % 2 == 0) {
+//      console.log("Uneti broj je paran ");
+//  }
+//  if (a % 2 == 1) {
+//       console.log("Uneti broj je neparan");
+//     }
+
+// a = window.prompt("Unesite prvi broj");
+// b = window.prompt("Unesite drugi broj");
+// c = window.prompt("Unesite treci broj");
+// let d = Math.max(a, b, c);
+// console.log(d);
+
+// a = window.prompt("Unesite prvi broj");
+//  b = window.prompt("Unesite drugi broj");
+//  c = window.prompt("Unesite treci broj");
+//  var brojevi =[a, b, c];
+//  brojevi.sort();
+//  console.log(brojevi);
+
+
+// a = window.prompt("Unesite prvi broj");
+// b = window.prompt("Unesite drugi broj");
+// c = window.prompt("Unesite treci broj");
+// if ((a, b, c == 0)) {
+
+// }
+
+// a = "Monday";
+
+
+//  switch.log("Today is Monday");
+//      break;
+//      case "Tuesday":
+//          console.log("Today is Tuesday");
+//          break;
+//          default:
+//              console.log("Uneti broj nije tacan");
+//  }
+
+// a = prompt("Unesi broj od 1 do 12");
+// switch (a) {
+//     case "1":
+//     console.log(Januar);
+//     break;
+//     case "2":
+//     console.log(Februar);
+//     break;
+//     case "3":
+//     console.log(Mart);
+//     break;
+//     case "4":
+//     console.log(April);
+//     break;
+//     case "5":
+//     console.log(Maj);
+//     break;
+//     case "6":
+//     console.log(Jun);
+//     break;
+//     case "7":
+//     console.log(Jul);
+//     break;
+//     case "8":
+//     console.log(Avgust);
+//     break;
+//     case "9":
+//     console.log(Septembar);
+//     break;
+//     case "10":
+//     console.log(Oktobar);
+//     break;
+//     case "11":
+//     console.log(Novembar);
+//     break;
+//     case "12":
+//     console.log(Decembar);
+//     break;
+//     default:
+//         console.log("Poogresan unos");
+//         }
+
+
+// console.log("Sejla".length);
+
+
+// a = "Sejla"
+// console.log([2]);
+
+// text ="Please visit and Microsoft!";
+
+// console.log(text.replace("Microsift","Zamenjeno");
+
+
+//  text ="Please visit and Microsoft!";
+
+//  console.log(text.slice(4));
+
+
+
+//  text ="Pleas) visit and Microsoft!";
+
+//  console.log(text.substr(5, 9));
+
+
+
+// text ="Please visit and Microsoft!";
+// console.log(text.toUpperCase());
+// console.log()text.toLowerCase());
+
+
+// console.log("         asviuasbvuai              .trim");
+
+// text ="Pleas visit and Microsoft!";
+// console.log(text[1]);
+// console.log(text.charAt(0));
+// console.log(text,indexOf("P"));
+
+
+//  a = prompt("Unesi broj od 1 do 12");
+//  switch (a) {
+//      case "1":
+//      console.log(Januar);
+//      break;
+//      case "2":
+//      console.log(Februar);
+//      break;
+//      case "3":
+//      console.log(Mart);
+//    break;
+//      case "4":
+//      console.log(April);
+//      break;
+//    case "5":
+//     console.log(Maj);
+//      break;
+//      case "6":
+//      console.log(Jun);
+//      break;
+//      case "7":
+//      console.log(Jul);
+//      break;
+//      case "8":
+//     console.log(Avgust);
+//      break;
+//      case "9":
+//      console.log(Septembar);
+//      break;
+//      case "10":
+//      console.log(Oktobar);
+//      break;
+//      case "11":
+//      console.log(Novembar);
+//      break;
+//   case "12":
+//      console.log(Decembar);
+//     break;
+//      default:
+//          console.log("Poogresan unos");
+//       }
+
+
+// a = 34;
+// b = 118;
+// c = 65;
+
+// if (a + b > c && + c > b && b + c > a) {
+//     console.log("trougao je moguc");
+// } else {
+//     console.log("trougao je moguc");
+// }
+
+
+// let
+// var
+// const
+
+// // pocetna vrednost; krajnja vrednost; korak
+// for ( let i = 2; i < 0; i += 2) {
+// console.log("Hello world - ", i);
+// }
+
+
+// var max = 0;
+
+// for (let i = 0; i < 5; i++) {
+//     broj = Number(prompt("Unesite broj:"));
+//     console.log(broj);
+//     if (broj > max) {
+//         max = broj;
+//     }
+// }
+
+// console.log("max je:");
+// console.log(max);
+
+
+// var i;
+// var j;
+// var sum;
+// for (let i = 1; i <= 6; i++) {
+//     for (let j = 1; j < i; j++) {
+//         sum += "*";
+//     }
+//     console.log(sum);
+//     sum = "";
+// }
+
+// for (let i = 0; i < 5; i ++) {
+//     let red ="";
+
+
+
+
+// for (let i = 1; i <= 10; i++ ) {
+//     var red ="";
+//     for (let j = 1; j <= 10; j++) {
+//         red += `${i * j} \t`
+//     }
+//     console.log(red, "\n");
+// }
+
+// var pozTemp = 0;
+// var negTemp = 0;
+
+// var pozBr = 0;
+// var negBr = 0;
+
+// for ( let i = 1; i < 10; i++ ) {
+//     t = Number(prompt("Unesite temp:"));
+//     if (t > 0) {
+//         pozBr++;
+//         pozTemp += t;
+
+//     } else {
+//         negBr++;
+//         negTemp += t;
+
+//     }
+// }
+// console.log("Srednja pozitivna je: ", pozTemp / pozBr);
+// console.log("Srednja negativna je: ", negTemp / negBr);
+
+
+
+// var rec = "ratar";
+
+// // r a t a r
+// // 0 1 2 3 4 
+
+// console.log(rec[0]);
+// console.log(rec[1]);
+// console.log(rec[2]);
+// console.log(rec[3]);
+// console.log(rec[4]);
+
+
+// var rec = "ratar";
+
+//  // r a t a r
+//  // 0 1 2 3 4
+
+// for (let i = 0; i < Math.floor(rec.length /2); i++) {
+//     if (rec [i] !== rec[rec.length -1 -i]) {
+//         console.log("rec nije polindrome");
+//         break;
+//     }
+// if (i == Math.floor(rec.length /2) -1) {
+//     console.log("rec je polindrome");
+// }
+// }
+
+// let ans = 0;
+// for (let i = 999; i >= 100; i--) {
+//     for (let j = 999; j >= 100; j--) {
+//         let num = i - j; 
+//         let s = String(num);
+//         let rs = s.split("").reverse().join("");
+//         if (s === rs) {
+//             ans = Math.max(ans, num);
+//         }
+//     }
+// }
+
+// brojac = 0;
+
+
+// while (brojac < 10) {
+//     console.log(brojac);
+//     brojac++;
+// }
+
+
+ moja_lozinka = "shelly";
+
+ kor_lozinka = prompt("Unesite lozinku");
+
+ while (moja_lozinka !== kor_lozinka) {
+     kor_lozinka = prompt("Pogresna lozinka pokusajte ponovo: ");
+
+ }
+ alert("Tacna lozinka")
+
+
