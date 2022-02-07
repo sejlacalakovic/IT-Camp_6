9c4db40407322385ee532625de310137d4c3eb36
Sejla <calakovicsejla6@gmail.com>
2022-02-07 23:01:34 +0100

    feb7

--- vezba2.js
+++ vezba2.js
@@ -678,6 +678,17 @@ function addBinary(a, b) {
   console.log( addBinary( 51,12 ) ); //Should be 111111
   console.log( addBinary( 100, 0 ) ); //Should be 1100100
   
-  
-  
+
+
+
+
+
+
+
+  // 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+  // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+
+  function addBinary(a,b) {
+    return (a + b).toString(2);
   }
