764495757ee374a4773aba15dafce93e99530994
Sejla <calakovicsejla6@gmail.com>
2022-02-08 22:54:41 +0100

    feb8

--- vezba2.js
+++ vezba2.js
@@ -701,36 +701,57 @@
 
 
 
-  covek1 = {
-    name: "sejla",
+//   covek1 = {
+ 
+//   //covek1 =  poajdwiQJFq[dfwjQFWJ;
+
+//     name: "sejla",
+//     surname: "calakovic",
+//    birhdate: "05.02.2001",
+//    // location: "NP",
+//    loacation: "NP",
+//    // job = qhw1hawfjdo12i44r
+//    function() {
+//      console.log("Doing some job");
+//    },
+//   };
+
+
+//   covek2 = { ...covek1};
+
+
+// //covek1 = poajdwiQJFq[dfwjQFWJ
+// covek2 = covek1 //poajdwiQJFq[dfwjQFWJ
+
+
+//   // console.log(covek1);
+//   // console.log(covek2);
+
+//    covek2.name = "Novo ime!!!";
+
+//    //console.log("=================================");
+//    console.log(covek1);
+//    console.log(covek2);
+
+
+//   // console.log(Object.keys(covek1));
+//   // console.log(Object.values(covek2));
+
+
+//   covek1.job(); //job- pokazivac
+
+
+covek1 ={
+name: "sejla",
     surname: "calakovic",
    birhdate: "05.02.2001",
-   location: "NP",
-   job: function() {
-     console.log("Doing some job");
-   },
-  };
+   
+ loacation: {
+   contry: "SRB",
+city: "NP",
+    },
+   };
 
 
-  covek2 = { ...covek1};
 
 
-//covek1 = poajdwiQJFq[dfwjQFWJ
-covek2 = covek1 //poajdwiQJFq[dfwjQFWJ
-
-
-  // console.log(covek1);
-  // console.log(covek2);
-
-  // covek2.name = "Novo ime!!!";
-
-  // console.log("=================================");
-  // console.log(covek1);
-  // console.log(covek2);
-
-
-  console.log(Object.keys(covek1));
-  console.log(Object.values(covek2));
-
-
-  covek1.job();
