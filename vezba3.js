237af13457f5ec721f4b3544e39dcc0a8074e67b
Sejla <calakovicsejla6@gmail.com>
2022-02-14 22:23:13 +0100

    feb14

--- vezba3.js
+++ vezba3.js
@@ -224,97 +224,130 @@
 
 
 
-class Book {
-    constructor(title = "", genre, author) {
-      this.title = title;
-      this.genre = genre;
-      this.author = author;
-      this.read = false;
-      this.read_date = null;
-    }
-  }
-  
-  class BookLists {
-    constructor() {
-      this.bookShelf = [];
-      this.booksRead = 0;
-      this.notBooksRead = this.bookShelf.length - this.booksRead;
-      this.nextBook = null;
-      this.currBook = null;
-      this.lastBook = null;
-    }
-  
-    add(book) {
-      if (book instanceof Book) {
-        debugger;
-        this.bookShelf.push(book);
-  
-        for (let i = 0; i < this.bookShelf.length; i++) {
-          if (!this.bookShelf[i].read) {
-            this.currBook = this.bookShelf[i];
-            if (this.bookShelf[i + 1]) {
-              this.nextBook = this.bookShelf[i + 1];
-            } else {
-              this.nextBook = null;
-            }
-            break;
-          }
-        }
-      } else {
-        console.log("Invalid argument type");
-      }
-    }
-  
-    finishCurrentBook() {
-      for (let i = 0; i < this.bookShelf.length; i++) {
-        const book = this.bookShelf[i];
-        if (book.title === this.currBook.title) {
-          this.bookShelf[i].read = true;
-          this.bookShelf[i].read_date = new Date();
-          this.lastBook = this.currBook;
-          this.currBook = this.nextBook;
-          if (this.bookShelf[i + 1]) {
-            this.nextBook = this.bookShelf[i + 2];
-          }
-          break;
-        }
-      }
-    }
-  }
-  
-  b1 = new Book("HP1", "magic", "JKR");
-  b2 = new Book("HP2", "magic", "JKR");
-  b3 = new Book("HP3", "magic", "JKR");
-  b4 = new Book("HP4", "magic", "JKR");
-  b5 = new Book("HP5", "magic", "JKR");
-  
-  bl = new BookLists();
-  
-  bl.add(b1);
-  bl.add(b2);
-  bl.add(b3);
-  bl.add(b4);
-  bl.add(b5);
-  
-  console.log(bl.lastBook);
-  console.log(bl.currBook);
-  console.log(bl.nextBook);
-  
-  bl.finishCurrentBook();
-  
-  console.log("=========================");
-  console.log(bl.lastBook);
-  console.log(bl.currBook);
-  console.log(bl.nextBook);
-  
-  bl.finishCurrentBook();
-  bl.finishCurrentBook();
-  bl.finishCurrentBook();
-  bl.finishCurrentBook();
-  
-  console.log("=========================");
-  console.log(bl.lastBook);
-  console.log(bl.currBook);
-  console.log(bl.nextBook);
-  
-  console.log(bl.bookShelf);
+//class Book {
+//constructor(title = "", genre, author) {
+//       this.title = title;
+//       this.genre = genre;
+//       this.author = author;
+//       this.read = false;
+//       this.read_date = null;
+//     }
+//   }
+
+//   class BookLists {
+//     constructor() {
+//       this.bookShelf = [];
+//       this.booksRead = 0;
+//       this.notBooksRead = this.bookShelf.length - this.booksRead;
+//       this.nextBook = null;
+//       this.currBook = null;
+//       this.lastBook = null;
+//     }
+
+//     add(book) {
+//       if (book instanceof Book) {
+//         debugger;
+//         this.bookShelf.push(book);
+
+//         for (let i = 0; i < this.bookShelf.length; i++) {
+//           if (!this.bookShelf[i].read) {
+//             this.currBook = this.bookShelf[i];
+//             if (this.bookShelf[i + 1]) {
+//               this.nextBook = this.bookShelf[i + 1];
+//             } else {
+//               this.nextBook = null;
+//             }
+//             break;
+//           }
+//         }
+//       } else {
+//         console.log("Invalid argument type");
+//       }
+//     }
+
+//     finishCurrentBook() {
+//       for (let i = 0; i < this.bookShelf.length; i++) {
+//         const book = this.bookShelf[i];
+//         if (book.title === this.currBook.title) {
+//           this.bookShelf[i].read = true;
+//           this.bookShelf[i].read_date = new Date();
+//           this.lastBook = this.currBook;
+//           this.currBook = this.nextBook;
+//           if (this.bookShelf[i + 1]) {
+//             this.nextBook = this.bookShelf[i + 2];
+//           }
+//           break;
+//         }
+//       }
+//     }
+//   }
+
+//   b1 = new Book("HP1", "magic", "JKR");
+//   b2 = new Book("HP2", "magic", "JKR");
+//   b3 = new Book("HP3", "magic", "JKR");
+//   b4 = new Book("HP4", "magic", "JKR");
+//   b5 = new Book("HP5", "magic", "JKR");
+
+//   bl = new BookLists();
+
+//   bl.add(b1);
+//   bl.add(b2);
+//   bl.add(b3);
+//   bl.add(b4);
+//   bl.add(b5);
+
+//   console.log(bl.lastBook);
+//   console.log(bl.currBook);
+//   console.log(bl.nextBook);
+
+//   bl.finishCurrentBook();
+
+//   console.log("=========================");
+//   console.log(bl.lastBook);
+//   console.log(bl.currBook);
+//   console.log(bl.nextBook);
+
+//   bl.finishCurrentBook();
+//   bl.finishCurrentBook();
+//   bl.finishCurrentBook();
+//   bl.finishCurrentBook();
+
+//   console.log("=========================");
+//   console.log(bl.lastBook);
+//   console.log(bl.currBook);
+//   console.log(bl.nextBook);
+
+//   console.log(bl.bookShelf);
+
+
+
+
+
+var Macka = {
+    umor: 0,
+    glad: 0,
+    usamljenost: 0,
+    sreca: 0,
+}
+
+function igranje() {
+    this.glad += 5;
+    this.usamljenost += 5;
+    this.sreca += 20;
+}
+
+function jelo() {
+    this.glad -= 5;
+    this.sreca += 5;
+}
+
+function pet() {
+    this.usamljenost -= 5;
+    this.umor += 5;
+}
+
+function spavanje() {
+    this.umor -= 5;
+}
+
+
