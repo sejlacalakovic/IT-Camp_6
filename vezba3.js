5209f508b6f78286d98b89ff99d629ab2816e364
Sejla <calakovicsejla6@gmail.com>
2022-02-26 17:52:00 +0100

    feb

--- vezba3.js
+++ vezba3.js
@@ -616,18 +616,28 @@
 
 
 
-const persons = [
-    {firstname : "Malcom", lastname: "Reynolds"},
-    {firstname : "Kaylee", lastname: "Frye"},
-    {firstname : "Jayne", lastname: "Cobb"}
-  ];
- console.log(persons);
- 
- 
-  function getFullName(item) {
-    return [item.firstname,item.lastname].join(" ");
-  }
+// const persons = [
+//     {firstname : "Malcom", lastname: "Reynolds"},
+//     {firstname : "Kaylee", lastname: "Frye"},
+//     {firstname : "Jayne", lastname: "Cobb"}
+//   ];
+//  console.log(persons);
 
 
 
- 
+
+//   function getFullName(item) {
+//     return [item.firstname,item.lastname].join(" ");
+//   };
+
+
+//map - pravi novi niz
+
+
+const cars = ["BMW", 123, false, "Sab", 12.5, "Jps jedan element"]
+
+arr1 = cars.map((el) => {
+return el + "novi el"
+});
+
+console.log(arr1);
