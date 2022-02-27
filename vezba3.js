ba334862891f6c2c5d2dbf1fe122488d5e619788
Sejla <calakovicsejla6@gmail.com>
2022-02-27 14:43:41 +0100

    feb27

--- vezba3.js
+++ vezba3.js
@@ -727,3 +727,43 @@
 // // expected output: 10
 
 
+
+
+// function findMissing(arr1, arr2) {
+//     arr2.forEach((el) => {
+//         if (arr1.includes(el)) {
+//             arr1.splice(arr1.indexOf(el), 1);
+//         }
+//     });
+
+//     return arr1[0];
+// }
+
+// res = findMissing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]);
+// res2 = findMissing([4, 3, 3, 61, 8, 8], [8, 61, 8, 3, 4]);
+
+// console.log(res);
+// console.log(res2);
+
+// console.log(findMissing([0, 0, 0, 0, 0], [0, 0, 0, 0]),);
+
+
+
+
+
+const heros = [
+    { name: "Spider Man"}
+    { name: ""}
+    
+]
+
+
+
+
+
+
+
+
+
+
+// sledeci cas promise
