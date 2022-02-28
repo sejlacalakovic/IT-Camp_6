c907b5ae3f7e148ce7d9dcfe8133514b2f2c6a79
Sejla <calakovicsejla6@gmail.com>
2022-02-28 22:53:09 +0100

    feb

--- vezba3.js
+++ vezba3.js
@@ -772,5 +772,21 @@
 
 
 
+
+let myPromise = new Promise(function(myResolve, myReject) {
+    // "Producing Code" (May take some time)
+    
+      myResolve(); // when successful
+      myReject();  // when error
+    });
+  
+    // "Consuming Code" (Must wait for a fulfilled Promise)
+    myPromise.then(
+      function(value) { /* code if successful */ },
+      function(error) { /* code if some error */ }
+    );
+
+
+
 // objekat promise - predstavlja konacni kraj ili neuspeh asinhrone operacije i njenu konacnu vrednost.
 // sledeci cas promise
