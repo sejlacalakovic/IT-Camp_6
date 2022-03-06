d422a87d12bc8b3639b5bb2dae4de44ff1df07f0
Sejla <calakovicsejla6@gmail.com>
2022-03-06 09:57:59 +0100

    mart

--- vezba3.js
+++ vezba3.js
@@ -902,29 +902,70 @@
 
 //promise.all()
 
-var p1 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 1000, 'one');
-  });
-  var p2 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 2000, 'two');
-  });
-  var p3 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 3000, 'three');
-  });
-  var p4 = new Promise((resolve, reject) => {
-    reject('Ovaj promise je namerno odbijen');
-  });
-  var p5 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 4000, 'five');
-  });
+// var p1 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 1000, 'one');
+//   });
+//   var p2 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, 'two');
+//   });
+//   var p3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 3000, 'three');
+//   });
+//   var p4 = new Promise((resolve, reject) => {
+//     reject('Ovaj promise je namerno odbijen');
+//   });
+//   var p5 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 4000, 'five');
+//   });
   
-  Promise.all([p1, p2, p3, p4, p5]).then(values => {
-    console.log(values);
-  }).catch(reason => {
-    console.log("Nama svih sastojaka", reason);
-  });  // Vraća: "Ovaj promise je namerno odbijen"
+//   Promise.all([p1, p2, p3, p4, p5]).then(values => {
+//     console.log(values);
+//   }).catch(reason => {
+//     console.log("Nama svih sastojaka", reason);
+//   });  // Vraća: "Ovaj promise je namerno odbijen"
 
 
 
 
+//promise.race()
 
+var p1 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 500, "one");
+});
+var p2 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 100, "two");
+});
+
+Promise.race([p1, p2]).then(function(value) {
+  console.log(value); // "two"
+  // Both resolve, but p2 is faster
+});
+
+//-----------------------------------------------------
+var p3 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 100, "three");
+});
+var p4 = new Promise(function(resolve, reject) {
+  setTimeout(reject, 500, "four");
+});
+
+Promise.race([p3, p4]).then(function(value) {
+  console.log(value); // "three"
+  // p3 is faster, so it resolves
+}, function(reason) {
+  // Not called
+});
+//-----------------------------------------------------
+var p5 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 500, "five");
+});
+var p6 = new Promise(function(resolve, reject) {
+  setTimeout(reject, 100, "six");
+});
+
+Promise.race([p5, p6]).then(function(value) {
+  // Not called
+}, function(reason) {
+  console.log(reason); // "six"
+  // p6 is faster, so it rejects
+});
