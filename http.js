4c9f662ac511b15a48417ba6b833d3ca076cdf6c
Sejla <calakovicsejla6@gmail.com>
2022-03-07 09:22:18 +0100

    mart

--- http.js
+++ http.js
@@ -1,44 +1,60 @@
-HTTP
+// HTTP
 
 
 
-osnovni pojmovi HTTP-a:
+// osnovni pojmovi HTTP-a:
 
-HTTP (engl. HyperText Transfer Protocol) je
-mrežni protokol koji pripada sloju
-aplikacije
-HTTP omogućava komunikaciju između
-različitih host računara i klijenata
-• Ovaj protokol pretpostavlja vrlo malo o
-pojedinačnom sistemu.
+// HTTP (engl. HyperText Transfer Protocol) je
+// mrežni protokol koji pripada sloju
+// aplikacije
+// HTTP omogućava komunikaciju između
+// različitih host računara i klijenata
+// • Ovaj protokol pretpostavlja vrlo malo o
+// pojedinačnom sistemu.
+
+
+
+
+// MALO ISTORIJATA
+
+// • [1965.] hipertext Ted Nelson
+//  [1989.] Tim Berns Lee i njegov tim na
+// CERN kreirao HTTP protokol zajedno sa
+// pratećim tehnologijama klijenta i servera kao
+// deo projekta
+// Worl Wide Web
+// U početku samo GET a u odgovoru samo
+// HTML stranica.
+
+
+
+
+
+// MALO ISTORIJATA
+
+// Prva verzija HTTP protokola je imala samo jednu vrstu
+// zahteva, koja je nazvana GET, i kojom se zahtevala stranica sa
+// server
+//  [1991.] Prva dokumentovana verzija HTTP V0.9.
+// Prva dokumentovana verzija HTTP protokola HTTP
+// bila jeHTTPV0.9 (iz 1991. godine).
+// Lider kompanije „HTTP Working Group― DejvRedžet
+// želeo je da proširi protokol dodavanjem novih
+// operacija, metoda zahteva i novih linija u zaglavlju.
+//  Tako je, 1996. godine nastaoHTTP V1.0
 
 
 
 
 MALO ISTORIJATA
 
-• [1965.] hipertext Ted Nelson
- [1989.] Tim Berns Lee i njegov tim na
-CERN kreirao HTTP protokol zajedno sa
-pratećim tehnologijama klijenta i servera kao
-deo projekta
-Worl Wide Web
-U početku samo GET a u odgovoru samo
-HTML stranica.
-
-
-
-
-
-MALO ISTORIJATA
-
-Prva verzija HTTP protokola je imala samo jednu vrstu
-zahteva, koja je nazvana GET, i kojom se zahtevala stranica sa
-server
- [1991.] Prva dokumentovana verzija HTTP V0.9.
-Prva dokumentovana verzija HTTP protokola HTTP
-bila jeHTTPV0.9 (iz 1991. godine).
-Lider kompanije „HTTP Working Group― DejvRedžet
-želeo je da proširi protokol dodavanjem novih
-operacija, metoda zahteva i novih linija u zaglavlju.
- Tako je, 1996. godine nastaoHTTP V1.0
+-1997 HTTP/1.1 Podržani klijenti: Arena,
+Netscape 2.0, Netscap Navigator Gold,
+Mosaic, Lynx i Internet Explorer 2.0 40%
+svih korisnika je odmah prihvatilo novi
+standard
+Postoje podaci, u pojedinim radovima koji
+prikazuje da je čak 65% korisnika „prešlo―
+na novi standard.
+Zvanično, standard je objavljen u januaru
+1997 godine.
