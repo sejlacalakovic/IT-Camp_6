f4295d5c189f330084b796ce95cd34f3f1334231
Sejla <calakovicsejla6@gmail.com>
2022-03-07 09:20:15 +0100

    mart

---
+++ http.js
@@ -0,0 +1,22 @@
+HTTP
+
+
+
+osnovni pojmovi HTTP-a:
+
+HTTP (engl. HyperText Transfer Protocol) je
+mrežni protokol koji pripada sloju
+aplikacije
+HTTP omogućava komunikaciju između
+različitih host računara i klijenata
+• Ovaj protokol pretpostavlja vrlo malo o
+pojedinačnom sistemu.
+
+
+
+
+
+
+
+
+
