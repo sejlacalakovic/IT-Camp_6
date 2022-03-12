8baf1a8dd03625d1fed05e01b280437fa45fd3be
Sejla <calakovicsejla6@gmail.com>
2022-03-12 18:17:27 +0100

    closures

--- vezba4.js
+++ vezba4.js
@@ -673,5 +673,28 @@
 // }
 // getData();
 
-//catfact.ninja
-//3 da povuce
+
+
+//closures
+
+var e = 10;
+function sum(a) {
+    return function (b) {
+return function (c) {
+    return function (d) {
+return a + b + c +d + e
+    }
+}
+    }
+}
+
+
+f1 = sum(1)
+f2 = f1(2)
+f3 = f2(3)
+res = f3(4)
+
+
+console.log(res);
+
+console.log(sum(1)(2)(3)(4));
