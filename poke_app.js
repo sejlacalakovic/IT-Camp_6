9828250501b54f00c7cae38790eb3f1d8701088c
Sejla <calakovicsejla6@gmail.com>
2022-03-19 18:24:14 +0100

    poke_app

---
+++ poke_app.js
@@ -0,0 +1,23 @@
+//https://pokeapi.co/api/v2/ability
+
+
+let pokemonResource = {};
+let pokemonArr = [];
+
+
+const getPokeList = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=10&limit=10")
+    .then((res) => {
+        const r = res.json();
+        return r;
+    })
+    .then((r) => {
+        console.log(r);
+        pokemonResource = r;
+    });
+};
+
+
+const getPokemon = (URL) => {
+    fetch(URL)
+}
