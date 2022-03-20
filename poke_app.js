253ce6c85829604eb8974dfd1446fea703795052
Sejla <calakovicsejla6@gmail.com>
2022-03-20 12:50:37 +0100

    animation.css

--- poke_app.js
+++ poke_app.js
@@ -1,35 +1,38 @@
 //https://pokeapi.co/api/v2/ability
 
-
+window.onload = async function() {
 let pokemonResource = {};
-let pokemonArr = [];
+const pokemonInfo = [];
 
 
-const getPokeList = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?offset=10&limit=10")
-    .then((res) => {
-        const r = res.json();
-        return r;
-    })
-    .then((r) => {
-        console.log(r);
-        pokemonResource = r;
-    });
+const getPokeList = async () => {
+    try {
+        const r = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=10&limit=10");
+const res = await r.json();
+pokemonResource = res;
+    }catch (error) {
+        console.log(error);
+    }
 };
 
+const getPokemon = async (URL) => {
+    try {
+        const p = await fetch (URL);
+        const pokemon = await p.json();
+        pokemonInfo.push(pokemon)
+    }catch (error) {
+        console.log(error);
+    }
+    };
 
-const getPokemon = (URL) => {
-    fetch(URL)
-    .then((res) => {
-        const r = res.json();
-        return r;
+    await getPokeList();
 
-    })
-    .then((res) => {
-        pokemonArr.push(res);
-        });
+// for (let u  = 0; u < pokemonResource.results.length; u++) {
+    
+//     return getPokemon(p.url);
+
+// });
+
+await Promise.all(pokemonArr);
+console.log(pokemonInfo);
 };
-
-getPokeList();
-
-console.log(pokemonResource);
