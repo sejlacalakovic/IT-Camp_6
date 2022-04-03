1da61b4095fc07738488f29b04ffa84eb0a65d98
Sejla <calakovicsejla6@gmail.com>
2022-04-03 17:44:17 +0100

    calculator-example.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 it
     function nextInLine(arr, item) {
       // Only change code below this line
-      .push(item);
+      arr.push(item);
       var removed = arr.shift();
       return removed;
       // Only change code above this line
