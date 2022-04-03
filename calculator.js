21ed88e2bb2f2a9cb8aaf63bc8dfc5396b1886e1
Sejla <calakovicsejla6@gmail.com>
2022-04-03 17:47:31 +0100

    calculator-example.js

--- calculator.js
+++ calculator.js
@@ -8,7 +8,7 @@ it
     }
     
     // Setup
-    testArr = [1, 2, 3, 4, 5];
+    const testArr = [1, 2, 3, 4, 5];
     
     // Display code
-    console.log(nextInLine(testArr, 6));
+    (nextInLine(testArr, 6));
