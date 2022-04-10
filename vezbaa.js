4ea7c4d8115eda3898f756a440b82f2c169e3d8d
Sejla <calakovicsejla6@gmail.com>
2022-04-10 09:51:23 +0000

    example

--- vezbaa.js
+++ vezbaa.js
@@ -1,16 +1,3 @@
-function transpose(matrix) {
-    let arr = [];
-    for (let i = 0; i < matrix[0].slice().length; i++) {
-      let tempArr = [];
-      for (let j = 0; j < matrix.slice().length; j++) {
-        tempArr.push(matrix[j][i]);
-      }
-      arr.push(tempArr);
-    }
-    return arr;
-  }
-  transpose();
-
 
 function pattern(n) {
     let arr = [];
