130dfc07d81255343cc65a0d29c2c93864a6e572
Sejla <calakovicsejla6@gmail.com>
2022-04-10 10:08:04 +0000

    example

--- pokedex.js
+++ pokedex.js
@@ -1,11 +1,12 @@
 const pokedex = document.getElementById("pokedex");
+const promises = [];
 
 const fetchPokemon = () => {
-  const promises = [];
-  for (let i = 1; i <= 150; i++) {
+  for (let i = 1; i <= 50; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(url).then((res) => res.json()));
   }
+
   Promise.all(promises).then((results) => {
     const pokemon = results.map((result) => ({
       name: result.name,
@@ -21,11 +22,11 @@ const displayPokemon = (pokemon) => {
   console.log(pokemon);
   const pokemonHTMLString = pokemon
     .map(
-      (pokeman) => `
+      (pokemon) => `
         <li class="card">
-            <img class="card-image" src="${pokeman.image}"/>
-            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-            <p class="card-subtitle">Type: ${pokeman.type}</p>
+            <img class="card-image" src="${pokemon.image}"/>
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">Type: ${pokemon.type}</p>
         </li>
     `
     )
